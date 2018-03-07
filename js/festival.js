4435f81e1905545171c139b1b412e0cf3e063ed1
francisLiuX <liupabshi90@126.com>
2018-03-07 09:42:11 +0800

    project0307updata1

--- js/festival.js
+++ js/festival.js
@@ -1,45 +1,55 @@
 $(()=>{
 
     //************************进入hover出下拉******************************
-    $(".image_tit_txt").hover(
-        function(){
-            $(this).parent().parent().children("div:last-child").css({
-                display:"block",
-                "z-index":"999"
-            })
-
-        },
+    // $(".image_tit_txt").hover(
+    //     function(){
+    //         $(this).parent().children("div:last-child").css({
+    //             display:"block",
+    //             "z-index":"999"
+    //         })
+    //
+    //     },
         // function(){
         //     $(this).parent().parent().children("div:last-child").css({
         //         display:"none"
         //     })
         //
         // }
-    )
+    // )
     //**********************进入内容栏来回切换*****************************
     var LIWIDTH=248;
     $(".main_image_tit").mouseover(e=>{
         e.preventDefault();
         var $tar=$(e.target);
         // console.log($tar)
-        if($tar.is("a")){
-            var index=$tar.parent().index();
+        if($tar.is("a")) {
+            var index = $tar.parent().index();
             console.log(index);
-            $(".move_box").css("left",LIWIDTH*index);
-
+            $(".move_box").css("left", LIWIDTH * index);
+            if(index==""){
+                fun("data/getfestivalA.php");
+            }else if(index==1){
+                fun("data/getfestivalB.php");
+            }else if(index==2){
+                fun("data/getfestivalA.php");
+            }else if(index==3){
+                fun("data/getfestivalB.php");
+            }
+        }
+    });
     //************************下拉内容******************************
+    // var a="data/getfestivalA.php";
+    function fun(a){
+        $.get(a).then(data => {
+            "use strict";
+            var html = "";
+            // console.log(1)
 
-
-    $.get("data/getfestivalA.php").then(data => {
-        "use strict";
-        var html = "";
-        console.log(1)
-
-        for (var i = 0; i < data.length; i++) {
-            // console.log(data)
-            // console.log(data[0])
-            // console.log(data[i].pic)
-            html += ` <ul class="inner_list">
+            for (var i = 0; i < data.length; i++) {
+                // console.log(data)
+                // console.log(data[0])
+                // console.log(data[i].pic)
+                html += ` <ul class="inner_list">
                     <li class="inner_item">
                         <a href="" target="_blank" class="inner_picture">
                                 <img src="${data[i].pic}" alt="">
@@ -58,14 +68,15 @@ $(()=>{
                             </div>
                      </li>
                     </ul>`;
-        }
-        console.log(2);
-        var $img = $(".main_image_inn");
-        console.log(3);
-        $img.html(html);
-    })
+            }
+            // console.log(2);
+            var $img = $(".main_image_inn");
+            // console.log(3);
+            $img.html(html);
+        })
 
-        }
-    });
+    }
+    fun("data/getfestivalA.php");
+
 
 })
