1b1804ee56f6128646fd7d01d9113abe0987f69d
francisLiuX <liupabshi90@126.com>
2018-03-09 16:53:24 +0800

    project0309updata1

--- js/festival.js
+++ js/festival.js
@@ -62,20 +62,60 @@ $(()=>{
     fun("data/getfestivalA.php");
 
 //***********************lift****************************
-    $(function(){
-        $(window).scroll(()=>{
-            if($(document).scrollTop()>=800){
-                $(".main_fixed_inner").css({display:'block'});
-                // $(".fixed_lift_item").hover( {background: '#918888'});
-                $(".fixed_lift_item1").addClass('hover')
+//     $(function(){
+//         $(window).scroll(()=>{
+//             if($(document).scrollTop()>=800){
+//                 $(".main_fixed_inner").css({display:'block'});
+//                 // $(".fixed_lift_item").hover( {background: '#918888'});
+//                 $(".fixed_lift_item1").addClass('hover')
+//
+//             }else{
+//                 $(".main_fixed_inner").css({display:'none'});
+//             }
+//         })
+//     })
 
+    //楼层显示
+    var $floors=$(".floor");
+    // var w=window.innerHeight;
+    $(window).scroll(()=>{
+        var scrollTop=$(window).scrollTop();
+        var offsetTop=$("#f1").offset().top;
+        if(offsetTop<scrollTop+innerHeight/3)
+            //********电梯显示与隐藏***************
+            $('.main_fixed_inner').show();
+        else
+            $('.main_fixed_inner').hide();
+        //具体哪个楼层点亮
+        for(var f of $floors){
+            var $f=$(f);
+            var offsetTop=$f.offset().top;
+            if(offsetTop<scrollTop+innerHeight/2){
+                var i=$floors.index($f);
+                var $li=$(".fixed_lift_list").find(".fixed_lift_item:eq("+i+")");
+                //*********楼层颜色变化**************
+                $li.addClass("hover")
+                    .siblings().removeClass("hover");
+            }
+        }
+    //************点击电梯跳转到具体楼层*********************
+        $(".fixed_lift_list").on("click",".fixed_lift_item",function(){
+            var $li=$(this);
+            if(!$li.is(":last-child")){
+                //找到当前的li对应的楼层
+                var i=$li.index();
+                var offsetTop=$floors.eq(i).offset().top;
+                $("html,body").stop(true).animate({
+                    scrollTop:offsetTop-100
+                },500);
             }else{
-                $(".main_fixed_inner").css({display:'none'});
+                $("html,body").stop(true).animate({
+                    scrollTop:0
+                },500);
             }
         })
+
     })
 
-
-    var $lift=$("#")
 //******************尾********************
 })
